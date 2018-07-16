bff15bf8ec6a05fa2c0c98c2567a2efef2d1a391
drelias15 <drelias15@gmail.com>
2018-07-16 17:52:09 -0400

    first commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main (String[] args){
+
+        Scanner keyboard = new Scanner(System.in);
+        Random rand = new Random();
+        int guessNumber, secreteNumber;
+
+        System.out.println("I have chosen a number between 1 and 10. Try to guess it.");
+        guessNumber = keyboard.nextInt();
+        secreteNumber = 1 + rand.nextInt(10);
+        while (secreteNumber !=  guessNumber){
+
+
+            System.out.println("Your Guess is " + guessNumber + ". That is not correct." + " My number was " + secreteNumber + ".");
+            break;
+        }
+
+        if (secreteNumber == guessNumber)
+        System.out.println("Yor Guess is "+ guessNumber + ". That is correct." + " My number was " + secreteNumber + ".");
+    }
+}
